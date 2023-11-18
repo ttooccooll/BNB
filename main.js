fd1c1e63e473426573d8d31c9c9e5fe4bf483288
Jason <jasonbohio@gmail.com>
2023-11-18 11:30:03 -0500

    itselectric

---
+++ main.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow, screen } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+
+  mainWindow = new BrowserWindow({
+    width: width,
+    height: height,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow();
+});
--- script.js
+++ script.js
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function startGame() {
+    stopSad()
     playAudio();
     playAudioLizzy()
     resumeAnimations();
